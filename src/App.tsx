3131cb972955341b33e45afbe8fd3a1646d0adbe
Victor Falck-Næss <43470721+Swarmino@users.noreply.github.com>
2023-09-29 09:49:23 +0200

    egg timer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import About from './components/About';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
+import Contact from './pages/Contact';
+import Egg from './pages/Egg';
+import About from './pages/About';
 import NavBar from './components/Navbar';
 
 function App() {
@@ -12,8 +13,9 @@ function App() {
         <NavBar/>
         <Routes>
           <Route path="/" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects/>} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/egg" element={<Egg/>} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/Egg.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import EggTimer from '../components/EggTimer';
+
+const Egg: React.FC = () => {
+    return (
+        <div>
+        <EggTimer/>
+        </div>
+    );
+    }
+
+export default Egg;
