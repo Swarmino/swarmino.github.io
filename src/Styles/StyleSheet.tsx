55f19f6e555b5f90d051ed01499d9d72acacec0a
Victor Falck-Næss <43470721+Swarmino@users.noreply.github.com>
2024-01-16 23:49:55 +0100

    vite rebuild

--- src/Styles/StyleSheet.tsx
+++
@@ -1,6 +0,0 @@
-const Colors = {
-    primary: '#f7287b',
-    accent: '#c717fc',
-    };
-
-export default Colors;
---
+++ swarmino.github/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1> Currently under construction</h1>
+      <h3>Please check back later</h3>
+      <span className="loading loading-infinity loading-lg"></span>
+    </>
+  )
+}
+
+export default App
