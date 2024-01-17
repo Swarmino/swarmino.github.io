5e35bee815af3d7d04af834e0c69f769a34e00e8
Victor Falck-Næss <43470721+Swarmino@users.noreply.github.com>
2024-01-17 11:38:13 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ function App() {
 
   return (
     <>
+    <div>
       <h1> Currently under construction</h1>
       <h3>Please check back later</h3>
       <span className="loading loading-infinity loading-lg"></span>
+    </div>
+      <footer className='flex justify-start text-'>&copy; Victor Falck-Næss</footer>
     </>
   )
 }
